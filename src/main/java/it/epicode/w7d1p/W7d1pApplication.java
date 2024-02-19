c03348654bcf578628aa737da14bf9ee7a7c3945
LorenzoN13 <lorenzoneroni13@gmail.com>
2024-02-19 16:05:35 +0100

    Initial commit

---
+++ src/main/java/it/epicode/w7d1p/W7d1pApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.w7d1p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W7d1pApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W7d1pApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/w7d1p/W7d1pApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.w7d1p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W7d1pApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
